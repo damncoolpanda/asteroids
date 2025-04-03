6a7f7bc26c3eaee5e5a6daf79cda5437eb4f8557
damncoolpanda <planstorm@gmail.com>
2025-04-03 21:09:21 +0000

    'updatable' and 'drawable' groups added

--- main.py
+++ main.py
@@ -4,27 +4,34 @@ from player import Player
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT), display=1)
 dt = 0
-
+clock = pygame.time.Clock()
+FPS = 60
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     while True:
+        dt = clock.tick(FPS) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
-        player.draw(screen)
-        
 
+        for item in drawable:
+            item.draw(screen)
+        
         pygame.display.flip()
-        dt = pygame.time.Clock().tick(60) / 1000
-        player.update(dt)
+
+        updatable.update(dt)
+
 
 
 if __name__ == "__main__":
